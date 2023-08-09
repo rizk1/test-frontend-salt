fcc4c5b1ee9e87d9f3a7c52aa610c52803e3a29d
rizk1 <srizki4455@gmail.com>
2023-08-09 17:13:23 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Slider from './components/Slider';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <Slider />
     </>
   );
 }
