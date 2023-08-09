9ade77373144f1d8720f16651928ae290304fa56
rizk1 <srizki4455@gmail.com>
2023-08-09 20:16:39 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import CoreList from './components/CoreList';
+import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Slider from './components/Slider';
+import Specialy from './components/Specialy';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Navbar />
       <Hero />
       <Slider />
+      <CoreList />
+      <Specialy />
+      <Footer />
     </>
   );
 }
